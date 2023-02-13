a66405dfd73f90a32f5f784798222bf376265110
Dima <skaizy44@gmail.com>
2023-02-13 17:31:49 +0200

    dsd
    sdsd

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 const sections = document.querySelectorAll(".section");
 const allLink = document.querySelectorAll(".nav__link");
 const menu = document.querySelector('.nav__list');
+const progressBar = document.querySelector(".progress__bar");
+const allSection = document.querySelectorAll('.section');
+const hello = document.querySelector('.hello')
+const helloAll = document.querySelectorAll('.hello');
+
+
+helloAll.forEach( hello => {
+    hello.style.top = Math.floor(Math.random() * 100) + 1 + "%";
+    hello.style.left = Math.floor(Math.random() * 100) + 1 + "%";
+})
 
 
 // функция обзервера, которая просматривает вхождение в блоки
@@ -15,7 +25,20 @@ const observer = new IntersectionObserver( (entries) => {
                     link.classList.add('nav__active')
                 }
             })
+//навешиваю анимацию на секцию
+            allSection.forEach(link => {
+                link.childNodes[1].classList.remove('hello')
+                
+                
+
+                if(link.id === entry.target.id){
+                    link.childNodes[1].style.top = "50%"
+                    link.childNodes[1].style.left = "50%"  
+                    link.childNodes[1].classList.add('hello')
+                }
+            })
         } 
+        
 
 	});
 },{
@@ -44,11 +67,19 @@ menu.addEventListener('click', (e) => {
 // функция анимации прогресс бара
 function animationProgressBar(){
     // значение скрола от верха страницы
+    const scrollPageHeight = document.documentElement.scrollTop;
     // узнать высоту всего документа(вся страница)
+    const pageHeight = document.documentElement.scrollHeight
+    //высота экрана пользователя
+    const screenHeight = document.documentElement.clientHeight;
     // узнать разницу высоты сайта и высоты экрана
-    //задать процент прокрутки
+    const heightDifference = pageHeight - screenHeight;
+    // процент прокрутки
+    const scrollPercent = (scrollPageHeight / heightDifference) * 100;
     // задать стили прогресс бару
+    progressBar.style.width = scrollPercent + '%'
 }
-animationProgressBar()
- 
+window.addEventListener("scroll", animationProgressBar)
+
+// console.log(screenHeight)
 
